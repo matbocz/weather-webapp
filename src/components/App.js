19f80f7c7d0a9cf80d9d3698227ee6274319151d
Mateusz Boczarski <matbocz@outlook.com>
2020-07-16 22:36:35 +0200

    Create footer

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,16 @@ function App() {
     return (
         <div className="container">
             <Weather />
+
+            <footer className="fixed-bottom">
+                <div className="text-center py-4 bg-primary text-white">
+                    &copy; 2020 Copyright:
+                    <a href="https://github.com/matbocz" className="text-white">
+                        {" "}
+                        Mateusz Boczarski
+                    </a>
+                </div>
+            </footer>
         </div>
     );
 }
