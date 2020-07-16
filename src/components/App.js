f15cedf35573e29fe14da2b01d1c175ce33a1287
Mateusz Boczarski <matbocz@outlook.com>
2020-07-16 12:28:56 +0200

    Change the application structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/superhero/bootstrap.min.css";
+import Weather from "./Weather";
+
+function App() {
+    return (
+        <div className="container">
+            <Weather />
+        </div>
+    );
+}
+
+export default App;
