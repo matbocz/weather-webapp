39b71660582ac07c40553614112c89d963f5fa7a
Mateusz Boczarski <matbocz@outlook.com>
2020-07-16 12:49:40 +0200

    Create Form.js

---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Form = (props) => (
+    <form onSubmit={props.submit}>
+        <div className="form-group">
+            <input
+                id="inputCity"
+                className="form-control"
+                placeholder="Enter a city"
+                type="text"
+                value={props.city}
+                onChange={props.change}
+                aria-describedby="helpCity"
+            />
+            <small id="helpCity" className="form-text text-muted">
+                Please enter a valid city name
+            </small>
+        </div>
+
+        <button className="btn btn-primary">Search</button>
+    </form>
+);
+
+export default Form;
