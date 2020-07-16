1875793219e84a4c639fc73156fb05b76cd8d29f
Mateusz Boczarski <matbocz@outlook.com>
2020-07-16 16:13:21 +0200

    Create Weather.js

---
+++ src/components/Weather.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import Form from "./Form";
+import Result from "./Result";
+
+const APIKey = "0ad8cfe8f31c124b14fb76eb2da3c25e";
+
+class Weather extends Component {
+    state = {
+        inputValue: "",
+        error: false,
+        city: "",
+        description: "",
+        temp: "",
+        pressure: "",
+        humidity: "",
+        sunrise: "",
+        sunset: "",
+        wind: "",
+        icon: "",
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            inputValue: e.target.value,
+        });
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue}&appid=${APIKey}&units=metric`;
+
+        fetch(API)
+            .then((response) => {
+                console.log(response);
+                if (response.ok) {
+                    return response;
+                }
+                throw Error("Response error");
+            })
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    error: false,
+                    city: this.state.inputValue,
+                    description: data.weather[0].description,
+                    temp: data.main.temp,
+                    pressure: data.main.pressure,
+                    humidity: data.main.humidity,
+                    sunrise: data.sys.sunrise,
+                    sunset: data.sys.sunset,
+                    wind: data.wind.speed,
+                    icon: data.weather[0].icon,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                this.setState({
+                    error: true,
+                    city: this.state.inputValue,
+                });
+            });
+    };
+
+    render() {
+        return (
+            <>
+                <Form
+                    inputValue={this.state.inputValue}
+                    change={this.handleChange}
+                    submit={this.handleSubmit}
+                />
+
+                <Result weather={this.state} />
+            </>
+        );
+    }
+}
+
+export default Weather;
