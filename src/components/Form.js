f3d58e1ca6222b66dc363b241cfba4a1a4727921
Mateusz Boczarski <matbocz@outlook.com>
2020-07-17 16:12:39 +0200

    New form layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,23 +1,29 @@
 import React from "react";
+import searchIcon from "../icons/search.svg";
 
 const Form = (props) => (
-    <form onSubmit={props.submit}>
-        <div className="form-group">
-            <input
-                id="inputCity"
-                className="form-control"
-                placeholder="Enter a city"
-                type="text"
-                value={props.inputValue}
-                onChange={props.change}
-                aria-describedby="helpCity"
-            />
-            <small id="helpCity" className="form-text text-muted">
-                Please enter a valid city name
-            </small>
+    <form onSubmit={props.submit} className="form-row mt-4">
+        <div className="col-10">
+            <div className="form-group">
+                <input
+                    id="inputCity"
+                    className="form-control"
+                    placeholder="Enter a city"
+                    type="text"
+                    value={props.inputValue}
+                    onChange={props.change}
+                    aria-describedby="helpCity"
+                />
+                <small id="helpCity" className="form-text text-muted">
+                    Please enter a valid city name
+                </small>
+            </div>
         </div>
-
-        <button className="btn btn-primary">Search</button>
+        <div className="col">
+            <button className="btn btn-primary w-100">
+                <img src={searchIcon} alt="Search" />
+            </button>
+        </div>
     </form>
 );
 
