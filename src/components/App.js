783c97335ffa3edc626822d8e40c605ff23a2351
Mateusz Boczarski <matbocz@outlook.com>
2020-07-17 17:29:12 +0200

    Move footer to a separate component

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootswatch/dist/superhero/bootstrap.min.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="container">
             <Weather />
 
-            <footer className="fixed-bottom">
-                <div className="text-center py-4 bg-primary text-white">
-                    &copy; 2020 Copyright:
-                    <a href="https://github.com/matbocz" className="text-white">
-                        {" "}
-                        Mateusz Boczarski
-                    </a>
-                </div>
-            </footer>
+            <Footer />
         </div>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+    <footer className="fixed-bottom">
+        <div className="text-center py-4 bg-primary text-white">
+            &copy; 2020 Copyright:
+            <a href="https://github.com/matbocz" className="text-white">
+                {" "}
+                Mateusz Boczarski
+            </a>
+        </div>
+    </footer>
+);
+
+export default Footer;
